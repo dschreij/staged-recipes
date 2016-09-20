0d507737cc3d1947d91681e59118af7a95a58302
Daniel Schreij <dschreij@gmail.com>
2016-09-20 23:16:46 +0200

    Process feedback of @patricksnape

--- recipes/sdl2/bld.bat
+++ recipes/sdl2/bld.bat
@@ -1,38 +1,30 @@
-:: MSVC library for Python 2.7 is not found automaticaly if it is 
-:: installed in the user's AppData folder. Add it to PATH
 setlocal EnableDelayedExpansion
 
+:: Configure for Release (results in smaller .dll file)
 set TARGET=Release
-:: Set target back to Debug for 64-bits builds with VS2008 and VS2010
+:: Set target back to None for 64-bits builds with VS2008 and VS2010
 if %VS_MAJOR% LSS 14 (
 	if %ARCH% == 64 (
 		set TARGET=None
 	)
 )
 
-:: When installing Visual C++ compiler tools for Python 2.7, all files are placed in the users AppData folder
-:: conda-bld tries to call vcvarsall at its classic location, which of course fails, so we have to call it from its
-:: new location in the the users appdata folder manually, but only if this folder exists of course.
+:: DirectX being integrated in the windows SDK and not being installed separately
+:: confuses good old Visual Studio 2008, so disable DirectX detection durion compilation.
 if "%PY_VER%"=="2.7" (
-	set VC9DIR="%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0"
-	:: set DXSDK_DIR="%programfiles(x86)%\Microsoft SDKs\Windows\v7.1A" :: Doesn't really work, only finds directx partially
-	IF EXIST !VC9DIR! ( 
-		if %ARCH% == 32 set VC_ARCH=x86
-		if %ARCH% == 64 set VC_ARCH=amd64
-		call !VC9DIR!\vcvarsall.bat !VC_ARCH!
-	)
 	set DIRECTX_FLAG="-DDIRECTX=OFF"
 )
 if errorlevel 1 exit 1
 
-:: Go to the source folder
-cd %SRC_DIR%
+:: Make a build folder and change to it.
 mkdir build
 cd build
 
-call %LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=!TARGET! !DIRECTX_FLAG! .. 
+:: Configure using the CMakeFiles
+call %LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=!TARGET! !DIRECTX_FLAG! .. 
 if errorlevel 1 exit 1
 
+:: Build!
 nmake
 if errorlevel 1 exit 1
 
